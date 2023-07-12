0496b40d75d41ec958741e419f4df22b88d8079b
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-07-12 06:49:51 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HighchartsChartModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/top-nav-bar/top-nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-top-nav-bar',
+  templateUrl: './top-nav-bar.component.html',
+  styleUrls: ['./top-nav-bar.component.scss']
+})
+export class TopNavBarComponent {
+}
